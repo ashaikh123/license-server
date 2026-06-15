3597e775bd599202a0d0a5e173a64bb01c6ffbe8
ashaikh123 <abdullah.gtalk@gmail.com>
2026-06-15 08:58:06 +0530

    Increase admin password hash length

--- models/admin.py
+++ models/admin.py
@@ -25,7 +25,7 @@ class Admin(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
